e212274fa974261b8c0620d67d80a07f8cb7cebd
iqabc <shota7145@gmail.com>
2019-02-10 21:57:26 +0900

    addHPmeter and name
    HPを縦棒にし,右側に名前を表示.
    HorizontalCube君が勝手にダメージを受けて表記が乱れるのが難点だが,characterがpublicなので仕方ない.

--- Assets/showHP.cs
+++ Assets/showHP.cs
@@ -8,6 +8,7 @@ public class showHP : MonoBehaviour {
 
     [SerializeField] private TextMeshProUGUI tmp;
     [NonSerialized] public Character character;
+    /////[NonSerialized] public Character character;
     private Text targetText;
 
     // Use this for initialization
@@ -22,7 +23,28 @@ public class showHP : MonoBehaviour {
         if (character != null)
         {
             tmp = GetComponent<TextMeshProUGUI>();
-            tmp.text = character.HP.ToString();
+
+            //HPを|||縦棒で表示. 100以上は | =50,100以下は | =5
+            tmp.text = "";
+            if (character.HP >= 100){
+                for(int i = 0;i <= character.HP / 50; i++)
+                {
+                    tmp.text += "|";
+                }
+            }
+            else if (character.HP>0)
+            {
+                for (int i = 0; i <= character.HP / 5; i++)
+                {
+                    tmp.text += "|";
+                }
+            }
+            else
+            {
+                tmp.text = "-----";
+            }
+            tmp.text += character.name;
+
         }
 	}
 }
