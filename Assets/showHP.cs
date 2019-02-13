18b667aba82e00985472dcd40bda16140f0265a1
iqabc <shota7145@gmail.com>
2019-02-13 23:46:07 +0900

    Revert "Merge pull request #10 from Cashimom/powerUp_showHP"
    This reverts commit da3a1fc5fcdeeccd884306a70b26d1d5d3113b96, reversing
    changes made to 1c0ea1e20edcf1e9458399e0dab4903c4a8c043f.

---
+++ Assets/showHP.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class showHP : MonoBehaviour {
+
+    [SerializeField] private TextMeshProUGUI tmp;
+    [NonSerialized] public Character character;
+    private Text targetText;
+
+    // Use this for initialization
+    void Start () {
+        if (tmp == null) ;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //targetText = this.GetComponent<Text>();
+
+        if (character != null)
+        {
+            tmp = GetComponent<TextMeshProUGUI>();
+            tmp.text = character.HP.ToString();
+        }
+	}
+}
