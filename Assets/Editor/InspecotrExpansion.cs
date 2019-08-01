052216a9bb77e4f135e688eaf55d386cf7d93be5
Cashimom <hiroshi2000yoshida@gmail.com>
2019-08-01 23:59:29 +0900

    ステージ自動生成β
    ステージを自動生成するやつを作り始めました
    stageGeneratorのインスペクタをエディター拡張で拡張していい感じにしました

---
+++ Assets/Editor/InspecotrExpansion.cs
@@ -0,0 +1,158 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+using System.Reflection;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(StageGenerator))]
+public class InspecotrExpansion : Editor
+{
+    StageGenerator expansion;
+    GameObject previewObject;
+    PreviewRenderUtility previewRenderUtility;
+    int previewNumber = 0;
+
+    private void OnEnable()
+    {
+        if(previewRenderUtility!=null)
+            previewRenderUtility.Cleanup();
+        expansion = target as StageGenerator;
+        previewSetup();
+    }
+
+    private void OnDisable()
+    {
+        if(previewRenderUtility!=null)
+            previewRenderUtility.Cleanup();
+        previewRenderUtility = null;
+        previewObject = null;
+    }
+
+
+    bool foldout = true;
+    int cnt = 0;
+    float a;
+
+    //インスペクタの描画
+    public override void OnInspectorGUI()
+    {
+
+        //base.OnInspectorGUI();
+        serializedObject.Update();
+        EditorGUI.BeginChangeCheck();
+        expansion.size= EditorGUILayout.Vector3IntField("Size", expansion.size);
+
+        //builtsSetting
+        if (foldout= EditorGUILayout.Foldout(foldout, ""))
+        {
+            //リストが足りてなかったら足す
+            if (expansion.builtsSetting.Count!= (int)StageGenerator.Built.MAX)
+            {
+                expansion.builtsSetting = new List<float>();
+                for (int i = expansion.builtsSetting.Count; i < (int)StageGenerator.Built.MAX; i++)
+                {
+                    expansion.builtsSetting.Add(1.0f);
+                }
+            }
+            //リストの表示
+            float s = 0.0f;
+            for (int i = 0; i < expansion.builtsSetting.Count; s += expansion.builtsSetting[i], i++) ;
+                for (var i =0;i< expansion.builtsSetting.Count;i++)
+            {
+                EditorGUILayout.BeginHorizontal();
+                EditorGUILayout.LabelField(((StageGenerator.Built)i).ToString(),GUILayout.MaxWidth(80));
+                expansion.builtsSetting[i] = EditorGUILayout.FloatField(expansion.builtsSetting[i]);
+                EditorGUILayout.LabelField((expansion.builtsSetting[i]/s*100).ToString("F1")+"%",GUILayout.MaxWidth(80));
+
+                if (GUILayout.Button("preview",GUILayout.MaxWidth(100)))
+                {
+                    previewNumber = i;
+                    changeObject();
+                }
+                EditorGUILayout.EndHorizontal();
+            }
+        }
+
+
+        EditorGUILayout.LabelField("test test");
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField("maji");
+        EditorGUILayout.LabelField("manzi");
+        EditorGUILayout.EndHorizontal();
+        if (EditorGUI.EndChangeCheck())
+        {
+            Undo.RecordObject(expansion, "stageGenerator");
+        }
+        serializedObject.ApplyModifiedProperties();
+    }
+
+    public override bool HasPreviewGUI()
+    {
+        return true;
+    }
+    public override GUIContent GetPreviewTitle()
+    {
+        return new GUIContent(((StageGenerator.Built)previewNumber).ToString());
+    }
+    //previewの描画
+    public override void OnInteractivePreviewGUI(Rect r, GUIStyle background)
+    {
+        if (previewRenderUtility == null)
+        {
+            previewSetup();
+        }
+        previewRenderUtility.BeginPreview(r, background);
+        previewObject.SetActive(true);
+
+        previewRenderUtility.camera.Render();
+
+        previewObject.SetActive(false);
+
+        previewRenderUtility.EndAndDrawPreview(r);
+
+        //Debug.Log(previewObject);
+
+    }
+
+    //previewの用意
+    private void previewSetup()
+    {
+        previewObject = expansion.getBuiltObject((StageGenerator.Built)previewNumber);
+
+        //previewObject.hideFlags = HideFlags.HideAndDontSave;
+        previewObject.SetActive(true);
+        var flags = BindingFlags.Static | BindingFlags.NonPublic;
+        var propInfo = typeof(Camera).GetProperty("PreviewCullingLayer", flags);
+        int previewLayer = (int)propInfo.GetValue(null, new object[0]);
+
+        previewRenderUtility = new PreviewRenderUtility(true);
+        previewRenderUtility.AddSingleGO(previewObject);
+        var previewCamera = previewRenderUtility.camera;
+        previewCamera.transform.position = previewObject.transform.position + new Vector3(2.5f, 2.5f, -5);
+        previewCamera.transform.LookAt(previewObject.transform);
+        //FieldOfView を 30 にするとちょうどいい見た目になる
+        previewRenderUtility.cameraFieldOfView = 30f;
+        //必要に応じて nearClipPlane と farClipPlane を設定
+        previewRenderUtility.camera.nearClipPlane = 0.3f;
+        previewRenderUtility.camera.farClipPlane = 1000;
+        //previewLayer のみ表示する
+        previewRenderUtility.camera.cullingMask = 1 << previewLayer;
+        previewObject.layer = previewLayer;
+        foreach (Transform transform in previewObject.transform)
+        {
+            transform.gameObject.layer = previewLayer;
+        }
+    }
+
+    //previewを作り直す
+    void changeObject()
+    {
+        if (previewRenderUtility != null)
+            previewRenderUtility.Cleanup();
+        previewRenderUtility = null;
+        previewObject = null;
+        previewSetup();
+    }
+}
---
+++ Assets/StageGenerator.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//マップを自動で生成する
+public class StageGenerator : MonoBehaviour
+{
+    [SerializeField] public Vector3Int size = new Vector3Int(5,5,5);
+
+    private int[,,] map;
+
+    private Vector3 blockSize;
+
+    private Vector3 firstPos;
+
+    public class BuiltSetting
+    {
+        float probability = 1.0f;
+    }
+
+    //[SerializeField] public List<float> builtSetting= new List<float>((int)Built.MAX);
+
+    public List<float> builtsSetting;
+
+    public enum Built:int
+    {
+        NULL,
+        N1,
+        N2,
+        N3,
+        MAX
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        firstPos = transform.position;
+        //blockSize = size;
+        blockSize.x = transform.localScale.x/size.x;
+        blockSize.y = transform.localScale.y/size.y;
+        blockSize.z = transform.localScale.z/size.z;
+        map = new int[size.x, size.y,size.z];
+        float sum = 0.0f;
+        for(int i = 0; i < builtsSetting.Count; i++)
+        {
+            sum += builtsSetting[i];
+        }
+        for (int x = 0; x < size.x; x++)
+        {
+            for(int y = 0; y < size.y; y++)
+            {
+                for (int z = 0; z < size.z; z++)
+                {
+                    float value = Random.value;
+                    float s = 0;
+                    int i;
+                    for (i = 0, s = builtsSetting[i] / sum; s < value; i++, s += builtsSetting[i] / sum);
+                    map[x, y, z] = i;
+                    if (map[x, y, z] != 0)
+                    {
+                        generateBuilt(this.vec3(x, y, z), (Built)map[x, y, z]);
+                    }
+
+                }
+                
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    Vector3 localPos(Vector3 pos)
+    {
+        return new Vector3(transform.localScale.x/size.x*(pos.x),
+            transform.localScale.y / size.y * (pos.y),
+            transform.localScale.z / size.z * (pos.z));
+    }
+
+    void generateBuilt(Vector3 pos,Built built)
+    {
+        GameObject g=getBuiltObject(built);
+        g.transform.parent = this.transform;
+        g.transform.position += localPos(pos)+firstPos;
+
+        g.transform.localScale = this.vec3(
+             g.transform.localScale.x * blockSize.x,
+             g.transform.localScale.y * blockSize.y,
+             g.transform.localScale.z * blockSize.z);
+
+    }
+
+    public GameObject getBuiltObject(Built built)
+    {
+        GameObject g = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        g.transform.position = this.vec3(0, 0, 0);
+        g.transform.Rotate(0, 0, 0);
+        g.transform.localScale = this.vec3(1, 1, 1);
+        g.name = "_StageObject";
+        
+        switch (built)
+        {
+            case Built.NULL:
+                g.transform.localScale *= 0;
+                break;
+            case Built.N1:
+                g.transform.localScale = this.vec3(0.8f, 0.05f, 0.8f);
+                break;
+            case Built.N2:
+                g.transform.localScale = this.vec3(1,1,1);
+                break;
+            case Built.N3:
+                g.transform.Rotate(0, 45, 0);
+                g.transform.localScale = this.vec3(1/1.41f, 1 / 1.41f, 1 / 1.41f);
+                break;
+            default:
+                g.transform.localScale = this.vec3(1,1,1);
+                break;
+        }
+        return g;
+    }
+}
