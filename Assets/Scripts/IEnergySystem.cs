cab9c882bbb5217d84b5202d04f0fa7da8896143
DakaⅡ <hiroshi2000yoshida@gmail.com>
2021-05-03 01:50:08 +0900

    create intarface of energy and health

---
+++ Assets/Scripts/IEnergySystem.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IEnergySystem
+{
+    public bool Consume(float value);
+    public bool CanConsume(float value);
+    public void Charge(float value);
+}
---
+++ Assets/Scripts/IHealthSystem.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public interface IHealthSystem
+{
+    public bool Damage(float value);
+    public void Recovery(float value);
+}
